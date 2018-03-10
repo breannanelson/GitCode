131e186397e0d7e08b3f6f97be74f13d4df39f2b
Breanna Nelson <breanna_a_nelson@hotmail.com>
2018-03-10 11:34:00 -0800

    Added sign out button

--- assets/script.js
+++ assets/script.js
@@ -1,96 +1,96 @@
-  // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyAUT3mY8ly8eMeZ6ZJAbHlj3b_UK1KTJwo",
     authDomain: "cocode-5453e.firebaseapp.com",
     databaseURL: "https://cocode-5453e.firebaseio.com",
     projectId: "cocode-5453e",
     storageBucket: "cocode-5453e.appspot.com",
     messagingSenderId: "279389961862"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var provider = new firebase.auth.GithubAuthProvider();
+var provider = new firebase.auth.GithubAuthProvider();
 
 
 $("#login").on("click", function () {
 
-   provider.setCustomParameters({
-    'allow_signup': 'true'
-  });
+    provider.setCustomParameters({
+        'allow_signup': 'true'
+    });
 
-  console.log(firebase.auth().signInWithPopup(provider))
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
 
-    // $("#login").remove();
-    // $("#sign-out").html("<button id='signOutBTN'>Sign Out</button>");
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
 
 
 
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
 
-//   if (ui.isPendingRedirect()) {
-//     ui.start('#firebaseui-auth-container', uiConfig);
-//   }
+
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+
+    //   if (ui.isPendingRedirect()) {
+    //     ui.start('#firebaseui-auth-container', uiConfig);
+    //   }
 
 
 })
 
-  initApp = function() {
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        // User is signed in.
-        var displayName = user.displayName;
-        var email = user.email;
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var uid = user.uid;
-        var phoneNumber = user.phoneNumber;
-        var providerData = user.providerData;
-        console.log("I am signed in")
-        user.getIdToken().then(function(accessToken) {
-          document.getElementById('sign-in-status').textContent = 'Signed in';
-          document.getElementById('sign-in').textContent = 'Sign out';
-          document.getElementById('account-details').textContent = JSON.stringify({
-            displayName: displayName,
-            email: email,
-            emailVerified: emailVerified,
-            phoneNumber: phoneNumber,
-            photoURL: photoURL,
-            uid: uid,
-            accessToken: accessToken,
-            providerData: providerData
-          }, null, '  ');
-        });
-      } else {
-        // User is signed out.
-        document.getElementById('sign-in-status').textContent = 'Signed out';
-        document.getElementById('sign-in').textContent = 'Sign in';
-        document.getElementById('account-details').textContent = 'null';
-      }
-    }, function(error) {
-      console.log(error);
+initApp = function () {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var uid = user.uid;
+            var phoneNumber = user.phoneNumber;
+            var providerData = user.providerData;
+            $("#login").remove();
+            $("#sign-out").html("<button id='signOutBTN'>Sign Out</button>");
+            user.getIdToken().then(function (accessToken) {
+                document.getElementById('sign-in-status').textContent = 'Signed in';
+                document.getElementById('sign-in').textContent = 'Sign out';
+                document.getElementById('account-details').textContent = JSON.stringify({
+                    displayName: displayName,
+                    email: email,
+                    emailVerified: emailVerified,
+                    phoneNumber: phoneNumber,
+                    photoURL: photoURL,
+                    uid: uid,
+                    accessToken: accessToken,
+                    providerData: providerData
+                }, null, '  ');
+            });
+        } else {
+            // User is signed out.
+            document.getElementById('sign-in-status').textContent = 'Signed out';
+            document.getElementById('sign-in').textContent = 'Sign in';
+            document.getElementById('account-details').textContent = 'null';
+        }
+    }, function (error) {
+        console.log(error);
     });
-  };
+};
 
-  window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     console.log("Hello");
     initApp();
 
-  });
+});
 
 
 
