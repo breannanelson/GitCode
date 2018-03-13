5aa4134d69b9d989c8d07f51102b19d16203db11
Breanna Nelson <breanna_a_nelson@hotmail.com>
2018-03-12 19:23:58 -0700

    Loading separate messaging HTML file

--- assets/messaging.js
+++ assets/messaging.js
@@ -10,28 +10,28 @@
 //   firebase.initializeApp(config);
 
 // var database = firebase.database();
-// var chatData = database.ref("/chat");
-//  // CHAT LISTENERS
-//   // Chat send button listener, grabs input and pushes to firebase. (Firebase's push automatically creates a unique key)
-//   $("#chat-send").click(function(event) {
-//     event.preventDefault()
+var chatData = database.ref("/chat");
+ // CHAT LISTENERS
+  // Chat send button listener, grabs input and pushes to firebase. (Firebase's push automatically creates a unique key)
+  $("#chat-send").click(function(event) {
+    event.preventDefault()
   
-//     if ($("#chat-input").val() !== "") {
+    if ($("#chat-input").val() !== "") {
   
-//       var message = $("#chat-input").val();
+      var message = $("#chat-input").val();
 
-//       chatData.push({
-//         displayName: displayName,
-//         userID : uid,
-//         message: message,
-//         time: firebase.database.ServerValue.TIMESTAMP,
-//       });
+      chatData.push({
+        displayName: displayName,
+        userID : uid,
+        message: message,
+        time: firebase.database.ServerValue.TIMESTAMP,
+      });
   
-//       $("#chat-input").val("");
-//     }
-//   });
+      $("#chat-input").val("");
+    }
+  });
 
-//   chatData.on("child_added", function(snapshot) {
-//       var msg = snapshot.val();
-//       $("#chat-messages").append(msg.displayName + " :  " + msg.message + "  | " + moment(msg.time).format("hh:mm") + "<br>");
-//   });
+  chatData.on("child_added", function(snapshot) {
+      var msg = snapshot.val();
+      $("#chat-messages").append(msg.displayName + " :  " + msg.message + "  | " + moment(msg.time).format("hh:mm") + "<br>");
+  });
