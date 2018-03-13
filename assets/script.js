d13aa169eff25aed9a2ea7a9260b0bdce524760a
Breanna Nelson <breanna_a_nelson@hotmail.com>
2018-03-12 18:59:13 -0700

    Loading separate messaging HTML file

--- assets/script.js
+++ assets/script.js
@@ -149,41 +149,15 @@ window.addEventListener('load', initApp);
 $("#messagingLink").on("click", function () {
     console.log("Hello Messaging")
     // $("#bio").empty();
-    $("#messagingLink").url("message.html");
+    $.ajax({
+        url: "message.html",
+        method: "GET",
+    }).then(function (obj) {
+        console.log(obj)
+    });
 });
 
 
 
-// var searchTerm = $(this).attr("data-name");
 
-// var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=mejJaFWSR2V2aY31O2HJaYqpl1cqLjBn&q=" + searchTerm + "&limit=15&offset=0&rating=G&lang=en";
 
-// $.ajax({
-//     url: queryURL,
-//     method: "GET",
-// }).then(function (obj) {
-//     console.log(obj)
-//     console.log(obj.data)
-//     if (obj.data.length === 0) {
-//         swal("Im sorry, Giphy Gen couldn't find any results for " + searchTerm + ". Try a different emotion!");
-//         $("button[data-name='"+searchTerm+"']").remove();
-//     }
-//     else {
-//         //image
-//         for (var i = 0; i <= 10; i = i + 2) {
-//             $("#gif").append("<div class='row rounded gifRow'><div class='col-md-6'><img class='img-fluid pic' " +
-//                 "src='" + obj.data[i].images["480w_still"].url + "'" +
-//                 "data-still='" + obj.data[i].images["480w_still"].url + "'" +
-//                 "data-animate='" + obj.data[i].images.fixed_height.url + "'" +
-//                 "alt='Gif of " + searchTerm + "' data-state='still' style='width:300px;'><p style='padding-left:30px;'>Rated: " + obj.data[i].rating + "</p></div>" +
-//                 "<div class='col-md-6'><img class='img-fluid pic' " +
-//                 "src='" + obj.data[i + 1].images["480w_still"].url + "'" +
-//                 "data-still='" + obj.data[i + 1].images["480w_still"].url + "'" +
-//                 "data-animate='" + obj.data[i + 1].images.fixed_height.url + "'" +
-//                 "alt='Gif of " + searchTerm + "' data-state='still' style='width:300px;'><p style='padding-left:30px;'>Rated: " + obj.data[i + 1].rating + "</p></div></div>");
-//         }
-//     }
-
-// })
-
-// });
